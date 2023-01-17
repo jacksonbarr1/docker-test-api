89ba51a9f0a98b8b46566ec77fabf7b408adee3b
Jackson Barr <jacksonbarr2021@gmail.com>
2023-01-17 17:35:25 -0500

    add JpaRepository interface

---
+++ src/main/java/com/example/dockertest/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.dockertest.repository;
+
+import com.example.dockertest.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}

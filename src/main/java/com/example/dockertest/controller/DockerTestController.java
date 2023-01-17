68d37d6943fbb52461397d00a15fd19a2de1543c
Jackson Barr <jacksonbarr2021@gmail.com>
2023-01-17 18:00:55 -0500

    add addCustomer method

--- src/main/java/com/example/dockertest/controller/DockerTestController.java
+++ src/main/java/com/example/dockertest/controller/DockerTestController.java
@@ -5,9 +5,7 @@ import com.example.dockertest.entity.Customer;
 import com.example.dockertest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DockerTestController {
@@ -21,6 +19,11 @@ public class DockerTestController {
     @GetMapping("/")
     public String home() {
         return "Hello Docker\nVersion: " + version;
+    }
+
+    @PostMapping("/customer")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return customerService.addCustomer(customer);
     }
 
     @GetMapping("/{id}")

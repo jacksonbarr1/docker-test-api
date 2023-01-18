4d06849ef37181ab20f219521acbd4f97c1891e0
Jackson Barr <jbarr39@gatech.edu>
2023-01-18 00:12:34 -0500

    add saveCustomer

--- src/main/java/com/example/dockertest/service/CustomerService.java
+++ src/main/java/com/example/dockertest/service/CustomerService.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface CustomerService {
     Customer findById(int id);
+
+    Customer saveCustomer(Customer customer);
 }
--- src/main/java/com/example/dockertest/service/CustomerServiceImpl.java
+++ src/main/java/com/example/dockertest/service/CustomerServiceImpl.java
@@ -8,9 +8,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
     private CustomerRepository customerRepository;
+
 
     public Customer findById(int id) {
         return customerRepository.findById(id).orElse(null);
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
     }
 }
